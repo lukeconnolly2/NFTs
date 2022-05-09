b0e0353be695f4011c2b772ff4ce8f6a96f18c32
lukeconnolly2 <72083100+lukeconnolly2@users.noreply.github.com>
2022-05-09 19:45:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import requests as r
+import json
+import pandas as pd
+
+def pingapi(address):
+    url = 'https://graphql.icy.tools/graphql'
+    header = {'x-api-key': '91a9fda75c224efaa7c44d6a9fff610c',
+              'content-type': 'application/json'
+              }
+
+    query = """
+      query CollectionStats($address: String!) {
+    contract(address: $address) {
+      ... on ERC721Contract {
+        stats {
+          floor
+          volume
+        }
+        name
+        attributes {
+          name
+          value
+        }
+      }
+    }
+  }
+  
+       """
+
+    address = {"address": address}
+    return json.loads(r.post(url, headers=header, json={'query': query, 'variables': address}).text)["data"]["contract"]
+
+
+def main():
+
+    address = input("Enter address: ")
+
+    res_obj = pingapi(address)
+
+    print(f"Name: {res_obj['name']}")
+    print(f'Floor: {res_obj["stats"]["floor"]}')
+    print(f'Volume: {res_obj["stats"]["volume"]}')
+    main()
+    return
+
+if __name__ == '__main__':
+    main()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
